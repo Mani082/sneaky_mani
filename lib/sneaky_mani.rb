c18d782d49c28a41c15d110f6d510f41509b5f21
mk185388 <mk185388@ncr.com>
2021-09-16 16:23:22 +0530

    add calculator

--- lib/sneaky_mani.rb
+++ lib/sneaky_mani.rb
@@ -1,11 +1,9 @@
 # frozen_string_literal: true
 
 require_relative "sneaky_mani/version"
+require_relative "sneaky_mani/builder"
 
 module SneakyMani
   class Error < StandardError; end
   # Your code goes here...
-  def greet_user(name)
-    puts "Hello #{name}, thanks for using my Gem"
-  end
 end
