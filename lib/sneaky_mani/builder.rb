9461e318db317af46710a615b797d099c2c418e3
mk185388 <mk185388@ncr.com>
2021-09-16 16:39:17 +0530

    Add back greetings

--- lib/sneaky_mani/builder.rb
+++ lib/sneaky_mani/builder.rb
@@ -1,38 +1,7 @@
-class String
-    def integer? 
-      return true if self =~ /^[1-9]\d*(\.\d+)?$/
-      false
+module SneakyMani
+  class Builder
+    def greet_user(name)
+        puts "Hello #{name}, thanks for using the Gem"
     end
   end
   
-  module SneakyMani
-  
-    class Builder
-      def initialize total:, gratuity:
-        @total = total
-        @gratuity = gratuity
-      end
-  
-      def generate
-        return calculation if number_based?
-        string_based
-      end
-  
-      def number_based?
-        (@gratuity.is_a? Numeric) || (@gratuity.integer?)
-      end
-  
-      def string_based
-        case @gratuity.downcase
-        when 'high'     then calculation 25
-        when 'standard' then calculation 18
-        when 'low'      then calculation 15
-        end
-      end
-  
-      def calculation gratuity = @gratuity
-        @total += @total * (gratuity.to_f / 100)
-      end
-    end
-  
-  end
